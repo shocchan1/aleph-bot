da13fec1d968fc14c4f279bd74597077927fad0c
AkunScript <dickypratama1097@gmail.com>
2026-07-25 10:21:12 +0700

    feat: add bot info command

---
+++ src/commands/utils/botinfo.js
@@ -0,0 +1,37 @@
+// @ts-check
+const { SlashCommandBuilder, EmbedBuilder, version } = require('discord.js');
+const client = require('../../client');
+
+module.exports = {
+    name: 'botinfo',
+    data: new SlashCommandBuilder()
+        .setName('botinfo')
+        .setDescription('Get information of this bot'),
+
+    async execute(interaction) {
+        const bot = interaction.client;
+        const user = interaction.user || interaction.member;
+
+        const embed = new EmbedBuilder()
+            .setTitle('**BOT INFORMATION**')
+            .setColor('DarkOrange')
+            .setDescription(`This bot used Discord.js v${version} & ${process.version}`)
+            .setFields(
+                { name: 'Name:', value: bot.user.displayName.toString || bot.user.globalName },
+                { name: 'Ping', value: `${client.ws.ping}ms` }
+            )
+            .setFooter({ text: `Requested by ${user.tag}`, iconURL: user.displayAvatarURL() })
+            .setTimestamp();
+
+        const pfp = bot.user.displayAvatarURL({ dynamic: true, size: 256 });
+        const banner = bot.user.bannerURL({ dynamic: true, size: 1024 });
+        if (pfp) {
+            embed.setThumbnail(pfp);
+        }
+        if (banner) {
+            embed.setImage(banner);
+        }
+
+        return interaction.reply({ embeds: [embed] });
+    }
+}
