c0390461b130f3b065f2c03392be11170428570b
AkunScript <dickypratama1097@gmail.com>
2026-07-25 10:50:05 +0700

    refactor: extract helper functions from botinfo command

--- src/commands/utils/botinfo.js
+++ src/commands/utils/botinfo.js
@@ -36,19 +36,3 @@ module.exports = {
         return interaction.reply({ embeds: [embed] });
     }
 }
-
-function botUptime(ms) {
-    const days = Math.floor(ms / (100 * 60 * 60 * 24));
-    const hours = Math.floor(ms / (1000 * 60 * 60));
-    const minutes = Math.floor(ms / (1000 * 60));
-    const seconds = Math.floor(ms / 1000);
-
-    const parts = [];
-
-    if (days) parts.push(`${days} day(s)`);
-    if (hours) parts.push(`${hours} hour(s)`);
-    if (minutes) parts.push(`${minutes} minute(s)`);
-    if (seconds || parts.length === 0) parts.push(`${seconds} second(s)`);
-
-    return parts.join(' ');
-}
