7f2dfa9f959df8171a0f6a67fb44ea5239b59596
AkunScript <dickypratama1097@gmail.com>
2026-07-25 09:53:40 +0700

    feat: add serverinfo command

---
+++ src/commands/utils/serverinfo.js
@@ -0,0 +1,38 @@
+// @ts-check
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'serverinfo',
+    data: new SlashCommandBuilder()
+        .setName('serverinfo')
+        .setDescription('Get server information'),
+
+    async execute(interaction) {
+        const guild = interaction.guild;
+        const user = interaction.user || interaction.member;
+
+        const embed = new EmbedBuilder()
+            .setTitle('**SERVER INFORMATION**')
+            .setColor('DarkOrange')
+            .setFields(
+                { name: 'Name:', value: `${guild.name} (${guild.id})` },
+                { name: 'Owner:', value: `<@${guild.ownerId}>` },
+                { name: 'Members:', value: guild.membersCount.toString() },
+                { name: 'Created At:', value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>` }
+            )
+            .setFooter({ text: `Requested by ${user.tag}`, iconURL: user.displayAvatarURL() })
+            .setTimestamp();
+
+        const banner = guild.bannerURL({ dynamic: true, size: 1024 });
+        if (banner) {
+            embed.setImage(banner)
+        }
+
+        const icon = guild.iconURL({ dynamic: true, size: 256 });
+        if (icon) {
+            embed.setThumbnail(icon);
+        }
+
+        return interaction.reply({ embeds: [embed] });
+    }
+}
