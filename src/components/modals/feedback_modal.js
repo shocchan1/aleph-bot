febe89d1ba5aab9a6d91fcd6aa4384b16b944019
AkunScript <dickypratama1097@gmail.com>
2026-07-26 11:30:10 +0700

    fix: get the value of feedback command

--- src/components/modals/feedback_modal.js
+++ src/components/modals/feedback_modal.js
@@ -6,6 +6,8 @@ module.exports = {
         const title = interaction.fields.getTextInputValue('feedback_title');
         const desc = interaction.fielads.getTextInputValue('feedback_description');
 
+        await interaction.channel.send({ content: `# **RECEIVED FEEDBACK**\n\nHey, hey, Master! Someone has sent feedback for us. Let's take a look, shall we?\n\nTitle:\n${title}\n\nDescription:\n${desc}` });
+
         return interaction.update({ content: 'Thanks you for the feedback!', components: [] });
     }
 }
