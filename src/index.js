5cbb80b6421c5ad5df33a74d1b15fa19f2152f17
AkunScript <dickypratama1097@gmail.com>
2026-07-26 07:34:16 +0700

    fix: added buttons collection

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const client = require('./client');
 const { loadCommands } = require('./handlers/cmdHandler');
 const { eventsLoader } = require('./handlers/eventHandler');
+const { buttonLoader } = require('./handlers/buttonLoader');
 
 client.commands = loadCommands('./src/commands');
 eventsLoader('./src/events', client);
+client.buttons = buttonLoader('./components/buttons');
 
 if (!process.env.TOKENLOGIN) return console.log('Void or undefined token');
 client.login(process.env.TOKENLOGIN);
