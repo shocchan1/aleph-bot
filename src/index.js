6d02abec36b205876721dacfb74b75123d4915ae
AkunScript <dickypratama1097@gmail.com>
2026-07-26 07:59:37 +0700

    fix: handling directory error not found

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { buttonLoader } = require('./handlers/buttonLoader');
 
 client.commands = loadCommands('./src/commands');
 eventsLoader('./src/events', client);
-client.buttons = buttonLoader('./components/buttons');
+client.buttons = buttonLoader('./src/components/buttons');
 
 if (!process.env.TOKENLOGIN) return console.log('Void or undefined token');
 client.login(process.env.TOKENLOGIN);
