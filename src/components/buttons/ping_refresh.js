dbc5a4d52d2f5179fb1ddc944bb06b6eb8de5573
AkunScript <dickypratama1097@gmail.com>
2026-07-26 07:56:54 +0700

    feat: added button content execution

---
+++ src/components/buttons/ping_refresh.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const { botUptime } = require('../../utils/botUptimeFormat');
+
+// @ts-check
+module.exports = {
+    customId: 'refresh_ping',
+
+    async execute(interaction) {
+        const bot = interaction.client;
+        const user = interaction.member || interaction.user;
+
+        const embed = new EmbedBuilder()
+            .setDescription('# **THANK YOU FOR CHECKING ME**\n\nI\'m good so don\'t worry about it.')
+            .setColor('DarkOrange')
+            .setFields(
+                { name: 'Ping:', value: botUptime(bot.ws.ping) }
+            )
+            .setFooter({ text: `Requested by ${user.username || user.tag}`, iconURL: user.displayAvatarURL() })
+            .setTimestamp();
+
+        const button = new ButtonBuilder()
+            .setCustomId('back_ping')
+            .setLabel('BACK')
+            .setStyle(ButtonStyle.Secondary);
+
+        const row = new ActionRowBuilder().addComponents(button);
+
+        await interaction.update({ embeds: [embed], components: [row] });
+    }
+}
