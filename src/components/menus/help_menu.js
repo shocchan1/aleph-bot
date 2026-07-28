06fdee438774c13f0b3322d3f7fdf404e7ab210b
AkunScript <dickypratama1097@gmail.com>
2026-07-28 20:38:08 +0700

    fix: removed unnecessary variables

--- src/components/menus/help_menu.js
+++ src/components/menus/help_menu.js
@@ -8,15 +8,12 @@ module.exports = {
         switch (selected) {
             case 'utility_menu':
                 return interaction.update({ content: 'Here are the utility commands that avaible.', components: [] });
-                break;
 
             case 'mod_menu':
                 return interaction.update({ content: 'Here are the moderation commands that avaible. This category can only be accessed by admins.', components: [] });
-                break;
 
             case 'fun_menu':
                 return interaction.update({ content: 'Here are the fun commands that avaible.', components: [] });
-                break;
         }
     }
 }
