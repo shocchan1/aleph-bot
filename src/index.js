447a9c4da5bdb2c6a0cbd3d29cdb685ec572d9c3
AkunScript <dickypratama1097@gmail.com>
2026-07-28 20:15:05 +0700

    feat: add client collection for menus

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const { loadCommands } = require('./handlers/cmdHandler');
 const { eventsLoader } = require('./handlers/eventHandler');
 const { buttonLoader } = require('./handlers/buttonLoader');
 const { modalLoader } = require('./handlers/modalHandler');
+const { menuStringLoader } = require('./handlers/menuStringLoader');
 
 client.commands = loadCommands('./src/commands');
 eventsLoader('./src/events', client);
 client.buttons = buttonLoader('./src/components/buttons');
 client.modals = modalLoader('./src/components/modals');
+client.menus =  menuStringLoader('./src/components/menus');
 
 if (!process.env.TOKENLOGIN) return console.log('Void or undefined token');
 client.login(process.env.TOKENLOGIN);
