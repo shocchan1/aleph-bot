e5c6dc598a4e514594f33491acfc0acd96249a8b
AkunScript <dickypratama1097@gmail.com>
2026-07-29 12:52:21 +0700

    feat: add function to channel menu command

---
+++ src/components/menus/channel_menu.js
@@ -0,0 +1,17 @@
+// @ts-check
+module.exports = {
+    customId: 'channelinfo_menu',
+
+    async execute(interaction) {
+        await interaction.deferUpdate();
+
+        const channelId = interaction.values[0];
+        const channel = interaction.guild.channels.cache.get(channelId);
+
+        if(!channel) return interaction.editReply({ content: 'Channel not found.', components: [] });
+
+        const reply = `**Channel Name:** ${channel.name}\n**Channel ID:** ${channel.id}\n**Channel Type:** ${channel.type}\n**Channel Topic:** ${channel.topic || 'No topic set.'}\n**Channel NSFW:** ${channel.nsfw ? 'Yes' : 'No'}\n**Channel Created At:** ${channel.createdAt.toLocaleString()}`;
+
+        return interaction.editReply({ content: reply, components: [] });
+    }
+}
