fea0eed8f143fd70315d119fe98b2ce7c48f0ac7
AkunScript <dickypratama1097@gmail.com>
2026-07-29 08:30:57 +0700

    feat: add profile menu function

---
+++ src/components/menus/profile_menu.js
@@ -0,0 +1,16 @@
+// @ts-check
+module.exports = {
+    customId: 'profile_user',
+
+    async execute(interaction) {
+        const userId = interaction.values[0];
+        const member = interaction.members.first(m => m.id === userId);
+
+        if (!member) return interaction.reply('Member not found.');
+
+        const user = member.user;
+        const profileInfo = `**Username:** ${user.username}\n**Nickname:** ${user.nickname || user.displayName}\n**ID:** ${user.id}\n**Created At:** ${user.createdAt.toDateString()}`;
+
+        return interaction.reply({ content: profileInfo });
+    }
+}
