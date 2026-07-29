c33c03bb0db7a84d7b430389618dc01035b6ecc0
AkunScript <dickypratama1097@gmail.com>
2026-07-29 12:56:02 +0700

    fix: handling channel menu error

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -6,7 +6,7 @@ module.exports = {
     once: false,
 
     async execute(interaction) {
-        if (interaction.isStringSelectMenu() || interaction.isUserSelectMenu() || interaction.isRoleSelectMenu() || interaction.isChanelSelectMenu()) {
+        if (interaction.isStringSelectMenu() || interaction.isUserSelectMenu() || interaction.isRoleSelectMenu() || interaction.isChannelSelectMenu()) {
             const menu = client.menus.get(interaction.customId);
             if (!menu) return console.warn(`[INTERACTION] No menu found for ${interaction.customId}`);
 
