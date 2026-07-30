7f2f280865ac04f80b703cd5d07bd69ac5828fc7
AkunScript <dickypratama1097@gmail.com>
2026-07-30 09:36:14 +0700

    fix: changed menu function switch case

--- src/components/menus/admin_user.js
+++ src/components/menus/admin_user.js
@@ -1,19 +1,48 @@
 // @ts-check
-const { UserSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
+const { UserSelectMenuBuilder, RoleSelectMenuBuilder, ChannelSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
-    customId: 'admin_user_menu',
+    customId: 'admin_menu',
 
     async execute(interaction) {
-        const userSelect = new UserSelectMenuBuilder()
-            .setCustomId('admin_user')
-            .setPlaceholder('Select a member...')
-            .setMinValues(1)
-            .setMaxValues(1)
-            .setRequired(true);
-            
-        const row = new ActionRowBuilder().addComponents(userSelect);
+        const values = interaction.values[0];
 
-        return interaction.update({ content: 'Select a member below to get their full information', components: [row] });
+        switch (values) {
+            case 'admin_user_menu':
+                const userMenu = new UserSelectMenuBuilder()
+                    .setCustomId('admin_user')
+                    .setMinValues(1)
+                    .setMaxValues(1)
+                    .setPlaceholder('Select a member...')
+                    .setRequired(true);
+
+                const row = new ActionRowBuilder().addComponents(userMenu);
+
+                return interaction.update({ content: 'Now, please select a member below to get their full information', components: [row] });
+
+            case 'admin_role_menu':
+                const roleMenu = new RoleSelectMenuBuilder()
+                    .setCustomId('admin_role')
+                    .setMinValues(1)
+                    .setMaxValues(1)
+                    .setPlaceholder('Select a role...')
+                    .setRequired(true);
+
+                const row1 = new ActionRowBuilder().addComponents(roleMenu);
+
+                return interaction.update({ content: 'Now, please select a role to get its full information.', components: [row1] });
+
+            case 'admin_channel_menu':
+                const channelMenu = new ChannelSelectMenuBuilder()
+                    .setCustomId('admin_channel')
+                    .setMinValues(1)
+                    .setMaxValues(1)
+                    .setPlaceholder('Select a channel to get its full information.')
+                    .setRequired(true);
+
+                const row2 = new ActionRowBuilder().addComponents(channelMenu);
+
+                return interaction.update({ content: 'Now, please select a channel to get its full information.', components: [row2] });
+        }
     }
 }
