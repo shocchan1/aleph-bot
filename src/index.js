ea80bd02f7ec0ea33db076055b8312a3e8ed458c
AkunScript <dickypratama1097@gmail.com>
2026-07-30 16:59:42 +0700

    feat: add asynchronous system for main file

--- src/index.js
+++ src/index.js
@@ -5,12 +5,25 @@ const { eventsLoader } = require('./handlers/eventHandler');
 const { buttonLoader } = require('./handlers/buttonLoader');
 const { modalLoader } = require('./handlers/modalHandler');
 const { menuLoader } = require('./handlers/menuLoader');
+const { connectDatabase } = require('./database/connection');
 
-client.commands = loadCommands('./src/commands');
-eventsLoader('./src/events', client);
-client.buttons = buttonLoader('./src/components/buttons');
-client.modals = modalLoader('./src/components/modals');
-client.menus =  menuLoader('./src/components/menus');
+(async () => {
+    try {
+        if (!process.env.TOKENLOGIN || !process.env.MONGO_URI) return console.log('Void or undefined token');
 
-if (!process.env.TOKENLOGIN) return console.log('Void or undefined token');
-client.login(process.env.TOKENLOGIN);
+        client.commands = loadCommands('./src/commands');
+        client.buttons = buttonLoader('./src/components/buttons');
+        client.modals = modalLoader('./src/components/modals');
+        client.menus =  menuLoader('./src/components/menus');
+        
+        eventsLoader('./src/events', client);
+
+        await connectDatabase();
+
+        await client.login(process.env.TOKENLOGIN);
+    } catch (error) {
+        console.warn('[INDEX] Failed to start the application.');
+        console.error(error);
+        process.exit(1);
+    }
+})();
